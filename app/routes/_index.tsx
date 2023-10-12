8d0226294d41a3e9b4fbd32ead39d99ee280c827
Dobromir Yordanov <32433021+dobromiryor@users.noreply.github.com>
2023-10-12 14:08:28 +0300

    refactor: 🚧 temporarily redirect from home to recipes route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,9 @@
-import { Role } from "@prisma/client";
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { useTranslation } from "react-i18next";
+import { redirect } from "@remix-run/node";
 
-import { auth } from "~/utils/auth.server";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-	const user = await auth.isAuthenticated(request);
-
-	return json({ isAuthenticated: !!user, isAdmin: user?.role === Role.ADMIN });
+export const loader = async () => {
+	return redirect("/recipes");
 };
 
 export default function IndexRoute() {
-	const { t } = useTranslation();
-	const { isAuthenticated, isAdmin } = useLoaderData<typeof loader>();
-
-	return (
-		<div className="flex flex-col items-start gap-2">
-			<span>{t("nav.home")}</span>
-		</div>
-	);
+	return null;
 }
