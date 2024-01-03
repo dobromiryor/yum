f7108eba72adf8a6d6e76da87ebbb1481508fce4
Dobromir Yordanov <32433021+dobromiryor@users.noreply.github.com>
2024-01-03 11:20:31 +0200

    fix: 🐛 fixed a slug bug
    Added a forbidden dictionary to protect routes such as `edit`, `new`, etc.

--- app/routes/recipes.edit.$recipeId.$lang.details.tsx
+++ app/routes/recipes.edit.$recipeId.$lang.details.tsx
@@ -30,6 +30,7 @@ import { Input } from "~/components/common/UI/Input";
 import { Multiselect } from "~/components/common/UI/Multiselect";
 import { Select } from "~/components/common/UI/Select";
 import { Textarea } from "~/components/common/UI/Textarea";
+import { FORBIDDEN_SLUGS } from "~/consts/forbidden-slugs.const";
 import { PARSED_ENV } from "~/consts/parsed-env.const";
 import { useFilteredValues } from "~/hooks/useFilteredValues";
 import i18next from "~/modules/i18next.server";
@@ -321,6 +322,26 @@ export const action = async ({
 		await parseFormData<FormData>(request.clone());
 
 	let success = true;
+
+	const t = await i18next.getFixedT(request);
+
+	if (FORBIDDEN_SLUGS.some((item) => item === rest.slug)) {
+		return json({
+			success: false,
+			message: t("error.slugExists"),
+		});
+	}
+
+	const foundRecipes = await prisma.recipe.findMany({
+		select: { slug: true },
+	});
+
+	if (foundRecipes.find((item) => item.slug === rest.slug)) {
+		return json({
+			success: false,
+			message: t("error.slugExists"),
+		});
+	}
 
 	await prisma.recipe
 		.update({
